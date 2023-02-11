511426d339613169050832b1827677a7e4e9a3dc
Natsu Ozawa <natsuozawa@outlook.com>
2023-02-11 13:24:36 -0500

    Fix test 4

--- test/Lock.js
+++ test/Lock.js
@@ -55,7 +55,8 @@ describe("Lock", function () {
       // We don't use the fixture here because we want a different deployment
       const latestTime = await time.latest();
       const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
+      const [receiver] = await ethers.getSigners();
+      await expect(Lock.deploy(latestTime, receiver.address, { value: 1 })).to.be.revertedWith(
         "Unlock time should be in the future"
       );
     });
